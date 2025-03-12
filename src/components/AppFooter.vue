827608e60dfc90c924aa5bbb62fa19191873a056
Patrick Audriaz (taaaupa3) <patrick.audriaz@swisscom.com>
2025-03-12 17:14:32 +0100

    Updating footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -2,21 +2,31 @@
   <footer class="app-footer">
     <div class="container">
       <p class="footer-text">
-        Created by Patrick Audriaz |
+        Created by Patrick Audriaz<br />
         <a href="mailto:audriazp@gmail.com" class="footer-link"
           >audriazp@gmail.com</a
         >
+        |
+        <a
+          href="https://www.instagram.com/petit_patoche/"
+          target="_blank"
+          class="footer-link"
+        >
+          @petit_patoche
+        </a>
       </p>
     </div>
   </footer>
 </template>
 
 <style lang="scss" scoped>
+@use "sass:color";
+
 .app-footer {
-  padding: 1rem 0;
+  padding: 0.7rem 0;
   background-color: #2c3e50;
   color: rgba(255, 255, 255, 0.8);
-  font-size: 0.9rem;
+  font-size: 0.85rem;
 
   .container {
     display: flex;
@@ -30,6 +40,7 @@
   .footer-text {
     margin: 0;
     text-align: center;
+    line-height: 1.5;
   }
 
   .footer-link {
@@ -38,7 +49,13 @@
 
     &:hover {
       text-decoration: underline;
+      color: color.adjust(white, $alpha: -0.2);
     }
+  }
+
+  .instagram-icon {
+    font-size: 1rem;
+    vertical-align: middle;
   }
 }
 </style>
